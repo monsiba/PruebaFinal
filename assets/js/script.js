c148d8a641190a33a63d861cbfd564f1dae3505c
Monserrat <monserrat.lba@gmail.com>
2021-08-20 21:19:27 -0400

    Header fixed, typeform edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+  $(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
